b344dc7f66e8f57691f1df16dd12344a659e8b98
renan_miguel2 <renanmiguel19900@gmail.com>
2026-03-22 02:37:05 -0300

    Adicionei o footer
    Apenas fiz o footer do site

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Car from "./components/Colorstocar/Car"
 import Description from "./components/Desc/Description"
 import Desempenho from "./components/Desempenho/Desempenho"
+import Footer from "./components/Footer/Footer"
 import Header from "./components/Hero/Header"
 import Hero from "./components/Hero/Hero"
 import Project from "./components/Project/Project"
@@ -18,6 +19,7 @@ function App() {
       <Project />
       <Desempenho />
       <Tecnologia />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <div className="mt-30">
+            <div className="mx-auto border-t border-zinc-800 mb-20 w-6/8"></div>
+            <div className="text-gray-400 text-center mb-5">
+                <p>&copy; 2026 Renan Miguel. Todos os direitos reservados.</p>
+            </div>
+        </div>
+    )
+}
+export default Footer
